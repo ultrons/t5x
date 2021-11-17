a8503c0384ff19c2411175825d6d2b989ddfa39e
Vaibhav Singh <sivaibhav@google.com>
2021-11-17 15:08:14 -0800

    experimental configs

--- sample-train-xxl-remat.sh
+++ sample-train-xxl-remat.sh
@@ -1,13 +1,13 @@
 #export TMPDIR=/mnt/t5x/temp
 export LIBTPU_INIT_ARGS=--xla_tpu_allow_sharding_on_minor_dim
-export EXP_PREFIX=xxl-no-mb-512
+export EXP_PREFIX=xxl-remat
 export ACCELERATOR_TYPE=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/accelerator-type -H "Metadata-Flavor: Google")
 export FLAX_PROFILE=1
 export MODEL_DIR="gs://sivaibhav-exp/t5x/t5x-models/benchmark/${ACCELERATOR_TYPE}-${EXP_PREFIX:=scale}"
 python3 t5x/train.py \
   --gin_search_paths=/home/sivaibhav/t5x \
-  --gin_file=/home/sivaibhav/t5x/t5x/configs/runs/pretrain-xxl.gin \
-  --gin_file=./t5x/examples/scalable_t5/t5_1_1/xxl.gin \
+  --gin_file=/home/sivaibhav/t5x/t5x/configs/runs/pretrain-xxl-remat.gin \
+  --gin_file=./t5x/examples/scalable_t5/t5_1_1/xxl-remat.gin \
   --gin.MIXTURE_OR_TASK_NAME="'wikipedia_20190301.en_v003_unsupervised'" \
   --gin.MIXTURE_OR_TASK_MODULE="'t5.data.tasks'" \
   --gin.MODEL_DIR="'${MODEL_DIR}'" \
