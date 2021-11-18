badabe4187960b3be91dfc5f7ec111b03c5cc05a
Vaibhav Singh <sivaibhav@google.com>
2021-11-18 13:44:16 -0800

    exp config

--- sample-train-decode-32b.sh
+++ sample-train-decode-32b.sh
@@ -1,6 +1,6 @@
 #export TMPDIR=/mnt/t5x/temp
 #export LIBTPU_INIT_ARGS=--xla_tpu_allow_sharding_on_minor_dim
-export EXP_PREFIX=32b
+export EXP_PREFIX=32bc
 export ACCELERATOR_TYPE=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/accelerator-type -H "Metadata-Flavor: Google")
 export FLAX_PROFILE=1
 export MODEL_DIR="gs://sivaibhav-exp/t5x/t5x-models/benchmark/${ACCELERATOR_TYPE}-${EXP_PREFIX:=scale}"
@@ -11,7 +11,7 @@ python3 t5x/train.py \
   --gin.MIXTURE_OR_TASK_NAME="'wikipedia_20190301.en_v003_unsupervised'" \
   --gin.MIXTURE_OR_TASK_MODULE="'t5.data.tasks'" \
   --gin.MODEL_DIR="'${MODEL_DIR}'" \
-  --gin.TASK_FEATURE_LENGTHS="{'inputs': 2048, 'targets': 2048}" \
+  --gin.TASK_FEATURE_LENGTHS="{'inputs': 1536, 'targets': 1536}" \
   --gin.TRAIN_STEPS=500 \
   --gin.DROPOUT_RATE=0.1 \
   --alsologtostderr \
