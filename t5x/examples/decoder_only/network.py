549d1dad13ed89cdf51c21694cc935b57a0afedb
Vaibhav Singh <sivaibhav@google.com>
2021-11-24 12:40:02 -0800

    exp config

--- t5x/examples/decoder_only/network.py
+++ t5x/examples/decoder_only/network.py
@@ -76,7 +76,7 @@ class DecoderLayer(nn.Module):
         name='relpos_bias')(l, l, False)
 
     # `inputs` is layer input with a shape [batch, length, emb_dim].
-    inputs = with_sharding_constraint(inputs, ('batch', 'length', 'embed'))
+    #inputs = with_sharding_constraint(inputs, ('batch', 'length', 'embed'))
     x = layers.LayerNorm(
         dtype=cfg.dtype, name='pre_self_attention_layer_norm')(
             inputs)
