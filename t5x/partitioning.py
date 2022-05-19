c5ef84ef747b2fdc09ddfbc420f448dd93b66125
Vaibhav Singh <sivaibhav@google.com>
2022-05-19 21:39:27 +0000

    edits for version updates

--- t5x/partitioning.py
+++ t5x/partitioning.py
@@ -31,10 +31,9 @@ import jax
 from jax import numpy as jnp
 from jax import random
 from jax.experimental.maps import Mesh
-from jax.experimental.maps import mesh
 from jax.experimental.pjit import pjit as jax_pjit
 from jax.experimental.pjit import with_sharding_constraint as jax_pjit_wsc
-from jax.interpreters.sharded_jit import PartitionSpec
+from jax.experimental import PartitionSpec
 import numpy as np
 from t5x import train_state as train_state_lib
 
@@ -621,13 +620,13 @@ class PjittedFnWithContext(PartitionedCallable):
     self._logical_axis_rules = logical_axis_rules
 
   def __call__(self, *args):
-    with mesh(self._mesh.devices,
+    with Mesh(self._mesh.devices,
               self._mesh.axis_names), flax_partitioning.axis_rules(
                   self._logical_axis_rules):
       return self._pjitted_fn(*args)
 
   def lower(self, *args):
-    with mesh(self._mesh.devices,
+    with Mesh(self._mesh.devices,
               self._mesh.axis_names), flax_partitioning.axis_rules(
                   self._logical_axis_rules):
       return self._pjitted_fn.lower(*args)
