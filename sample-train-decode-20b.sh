05ae3276feb5d3c7e0257fcaecdd4aa59ce25c32
Vaibhav Singh <sivaibhav@google.com>
2022-12-08 06:24:31 +0000

    debug

--- sample-train-decode-20b.sh
+++ sample-train-decode-20b.sh
@@ -12,7 +12,7 @@ python3 t5x/train.py \
   --gin.MIXTURE_OR_TASK_NAME="'wikipedia_20190301.en_v003_unsupervised'" \
   --gin.MIXTURE_OR_TASK_MODULE="'t5.data.tasks'" \
   --gin.MODEL_DIR="'${MODEL_DIR}'" \
-  --gin.TASK_FEATURE_LENGTHS="{'targets': 2048}" \
+  --gin.TASK_FEATURE_LENGTHS="{'targets': 1024}" \
   --gin.TRAIN_STEPS=60 \
   --gin.DROPOUT_RATE=0 \
   --alsologtostderr \
