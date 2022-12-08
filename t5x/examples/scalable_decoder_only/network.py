d157820e23e5c50987eee51655d7d188a36367b4
Vaibhav Singh <sivaibhav@google.com>
2022-12-08 23:27:07 +0000

    exp add mlp remat

--- t5x/examples/scalable_decoder_only/network.py
+++ t5x/examples/scalable_decoder_only/network.py
@@ -84,9 +84,8 @@ class DecoderLayer(nn.Module):
 
     # Self-attention block
     MHA = layers.MultiHeadDotProductAttention
-    #policy = None
-    # policy = None
-    policy = jax.checkpoint_policies.checkpoint_dots_with_no_batch_dims
+    policy = None
+    #policy = jax.checkpoint_policies.checkpoint_dots_with_no_batch_dims
     MHA = remat(  # pylint: disable=invalid-name
           MHA,
           prevent_cse=not cfg.scan_layers,
