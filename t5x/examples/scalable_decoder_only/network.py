25495f30b2555bdc454eff02d6c6b1205388f3aa
Vaibhav Singh <sivaibhav@google.com>
2022-12-09 03:49:50 +0000

    named remat

--- t5x/examples/scalable_decoder_only/network.py
+++ t5x/examples/scalable_decoder_only/network.py
@@ -235,8 +235,7 @@ class Decoder(nn.Module):
       if cfg.remat_policy == 'minimal':
         #policy = jax.checkpoint_policies.checkpoint_dots_with_no_batch_dims
         policy = jax.checkpoint_policies.save_only_these_names(
-          'combined_qkv_proj', 'query_proj', 'value_proj', 'key_proj',
-          'context', 'out_proj')
+          'out_proj', 'context')
         #policy = jax.checkpoint_policies.save_any_names_but_these(
         #  'combined_qkv_proj', 'query_proj', 'value_proj', 'key_proj',
         #  'context', 'out_proj')
