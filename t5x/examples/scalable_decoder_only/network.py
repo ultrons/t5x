ed7c7c15b48c6fe212d76151308ca8b7c0ecdcf0
Vaibhav Singh <sivaibhav@google.com>
2022-12-09 07:56:08 +0000

    debug

--- t5x/examples/scalable_decoder_only/network.py
+++ t5x/examples/scalable_decoder_only/network.py
@@ -131,7 +131,7 @@ class DecoderLayer(nn.Module):
     MLP = layers.MlpBlock
     #policy = None
     policy = jax.checkpoint_policies.save_only_these_names(
-          'mlp_int')
+         'mlp_out')
     MLP = remat(  # pylint: disable=invalid-name
           MLP,
           prevent_cse=not cfg.scan_layers,
