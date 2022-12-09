30ae06f056bc4c8e6bf2cc9bfca799b9cde8886e
Vaibhav Singh <sivaibhav@google.com>
2022-12-09 04:32:36 +0000

    qkv+contex+out+mlp-1

--- t5x/examples/scalable_decoder_only/network.py
+++ t5x/examples/scalable_decoder_only/network.py
@@ -235,7 +235,7 @@ class Decoder(nn.Module):
       if cfg.remat_policy == 'minimal':
         #policy = jax.checkpoint_policies.checkpoint_dots_with_no_batch_dims
         policy = jax.checkpoint_policies.save_only_these_names(
-          'context', 'out_proj', 'mlp_out')
+         'combined_qkv_proj', 'context', 'out_proj', 'mlp_out')
         #policy = jax.checkpoint_policies.save_any_names_but_these(
         #  'combined_qkv_proj', 'query_proj', 'value_proj', 'key_proj',
         #  'context', 'out_proj')
