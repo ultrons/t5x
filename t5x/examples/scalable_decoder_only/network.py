3c1fd4626b4bb036d445ea88f367becc60bc060d
Vaibhav Singh <sivaibhav@google.com>
2022-12-09 08:09:39 +0000

    debug

--- t5x/examples/scalable_decoder_only/network.py
+++ t5x/examples/scalable_decoder_only/network.py
@@ -88,7 +88,7 @@ class DecoderLayer(nn.Module):
     #policy = jax.checkpoint_policies.everything_saveable
     #policy = jax.checkpoint_policies.save_any_names_but_these(
     policy = jax.checkpoint_policies.save_only_these_names(
-         'context')
+         'combined_qkv_proj', 'context')
         #policy = jax.checkpoint_policies.save_any_names_but_these(
     MHA = remat(  # pylint: disable=invalid-name
           MHA,
